c655c15a83d01a2f8fe96c10698961119ddab37f
Anugnareddymule <144136995+Anugnareddymule@users.noreply.github.com>
2025-07-03 13:37:31 +0530

    Update aws-config.js

--- aws-config.js
+++ aws-config.js
@@ -5,6 +5,9 @@ AWS.config.update({
         IdentityPoolId: 'us-east-1_hEINciki8',
         clientId: '35nr2ouhvulntc32c76c619vff'
     })
+    api: {
+    invokeUrl:'https://g0n36aa73c.execute-api.us-east-1.amazonaws.com/dev',
+    }
 });
 
 export const dynamodb = new AWS.DynamoDB.DocumentClient();
